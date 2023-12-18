7d9253024564ab8321d989beb5db0b7f76895033
leandrocgsi <leandrocgsi@gmail.com>
2023-12-18 19:42:41 -0300

    Testing Generation of HATEOAS Links with Mockito - Model Mapper Version

--- 14_HATEOAS_Model_Mapper/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 14_HATEOAS_Model_Mapper/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,11 +1,9 @@
 package br.com.erudio.data.vo.v1
 
-import com.github.dozermapper.core.Mapping
 import org.springframework.hateoas.RepresentationModel
 
 data class PersonVO (
 
-    @Mapping("id")
     var key: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
--- 14_HATEOAS_Model_Mapper/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/mapper/DozerMapper.kt
+++ 14_HATEOAS_Model_Mapper/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/mapper/DozerMapper.kt
@@ -1,11 +1,31 @@
 package br.com.erudio.mapper
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder
-import com.github.dozermapper.core.Mapper
+import br.com.erudio.data.vo.v1.PersonVO
+import br.com.erudio.model.Person
+import org.modelmapper.ModelMapper
+import java.awt.print.Book
+import javax.print.attribute.standard.Destination
+
+
 
 object DozerMapper {
 
-    private val mapper: Mapper = DozerBeanMapperBuilder.buildDefault()
+    val mapper: ModelMapper = ModelMapper()
+
+    init {
+        mapper.createTypeMap<Person, PersonVO>(
+            Person::class.java,
+            PersonVO::class.java
+        )
+        .addMapping<Long>(Person::id, PersonVO::key)
+
+        mapper.createTypeMap<PersonVO, Person>(
+            PersonVO::class.java,
+            Person::class.java
+        )
+        .addMapping<Long>(PersonVO::key, Person::id)
+
+    }
 
     fun <O,D> parseObject(origin: O, destination: Class<D>?): D {
         return mapper.map(origin, destination)
